94a4b392e6486110fbc7792ff9e54230b8ca4bf6
Simo Kuparinen <simo.kuparinen@helsinki.fi>
2019-12-15 14:13:21 +0200

    update

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (res, req) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
+
     res.status(204).end()
 })
 
